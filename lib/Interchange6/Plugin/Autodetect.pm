f6630fe10772615a5b202022f28057bbcc847641
Marco Pessotto <melmothx@gmail.com>
2014-05-26 14:24:46 +0200

    Bumped version

--- lib/Interchange6/Plugin/Autodetect.pm
+++ lib/Interchange6/Plugin/Autodetect.pm
@@ -10,11 +10,11 @@ Interchange6::Plugin::Autodetect - The great new Interchange6::Plugin::Autodetec
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS

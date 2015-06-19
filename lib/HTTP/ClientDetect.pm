b43468381684a508dcf7f0cdf6571e1c7503a522
Stefan Hornburg (Racke) <racke@linuxia.de>
2015-06-19 10:00:26 +0200

    Bump up version number.

--- lib/HTTP/ClientDetect.pm
+++ lib/HTTP/ClientDetect.pm
@@ -10,11 +10,11 @@ HTTP::ClientDetect - Detect language and location of an HTTP request
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
